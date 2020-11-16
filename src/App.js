24a148e9811f531acb6c2b52bc201d36e115aae0
Thai Pham <jsxthai@gmail.com>
2020-11-16 20:36:45 +0700

    fix warning html

--- src/App.js
+++ src/App.js
@@ -3,18 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import Payin from './components/Payin/Payin';
 import History from './components/History/History';
-import Footer from './components/Footer/Footer';
 import CreateUser from './components/CreateUser/CreateUser';
 
 
 function App() {
-    console.log('app')
     return (
         <>
-            <Nav>
-                <Footer />
-
-            </Nav>
+            <Nav />
             <Switch>
                 <Route path='/' component={Dashboard} exact />
                 <Route path='/dashboard' component={Dashboard} exact />
--- src/components/History/History.js
+++ src/components/History/History.js
@@ -31,12 +31,15 @@ const History = () => {
                   <div className="table-responsive">
                     <table className="table">
                       <thead className=" text-primary">
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Country</th>
-                        <th>City</th>
-                        <th>Salary</th>
+                        <tr>
+                          <th>ID</th>
+                          <th>Name</th>
+                          <th>Country</th>
+                          <th>City</th>
+                          <th>Salary</th>
+                        </tr>
                       </thead>
+                      {/* data  */}
                       <tbody>
                         <tr>
                           <td>1</td>
@@ -51,34 +54,6 @@ const History = () => {
                           <td>Curaçao</td>
                           <td>Sinaai-Waas</td>
                           <td className="text-primary">$23,789</td>
-                        </tr>
-                        <tr>
-                          <td>3</td>
-                          <td>Sage Rodriguez</td>
-                          <td>Netherlands</td>
-                          <td>Baileux</td>
-                          <td className="text-primary">$56,142</td>
-                        </tr>
-                        <tr>
-                          <td>4</td>
-                          <td>Philip Chaney</td>
-                          <td>Korea, South</td>
-                          <td>Overland Park</td>
-                          <td className="text-primary">$38,735</td>
-                        </tr>
-                        <tr>
-                          <td>5</td>
-                          <td>Doris Greene</td>
-                          <td>Malawi</td>
-                          <td>Feldkirchen in Kärnten</td>
-                          <td className="text-primary">$63,542</td>
-                        </tr>
-                        <tr>
-                          <td>6</td>
-                          <td>Mason Porter</td>
-                          <td>Chile</td>
-                          <td>Gloucester</td>
-                          <td className="text-primary">$78,615</td>
                         </tr>
                       </tbody>
                     </table>
