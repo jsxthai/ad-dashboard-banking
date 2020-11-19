d3a5ddd570a9f572aaca0f5d3e8df5aa7c0ac0c3
Thai Pham <jsxthai@gmail.com>
2020-11-20 00:03:50 +0700

    Updates

--- src/api/account.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const url = 'http://localhost:7777/api/accounts';
-
-export const queryAccount = (accountNumber) => {
-    return axios.get(`${url}/${accountNumber}`)
-}
--- src/constants/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const LOGIN = 'LOGIN';
-export const CREATE_USER = 'CREATE_USER';
-export const FETCH_USERS = 'FETCH_USERS';
-export const PAY_IN = 'PAY_IN';
-export const LOGIN_FAIL = 'LOGIN_FAIL';
